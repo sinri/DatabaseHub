bd1a6328e5def07993e08f37fa1d3fc8fa20ea7c
Vinci <ivill@live.com>
2018-12-13 22:10:30 +0800

    save

--- frontend/static/component/TopBanner.js
+++ frontend/static/component/TopBanner.js
@@ -1,85 +1,97 @@
 let TopBannerComponent = {
-    template: '<i-menu mode="horizontal" theme="dark" @on-select="onMenuItemSelected" :active-name="opened">' +
-        '<template v-for="item in menuItems">' +
-        '<menu-item :name="item.name" :style="item.style+(item.name===opened?\'background: rgb(91, 100, 120);\':\'\')"><Icon :type="item.icon" /> {{item.text}}</menu-item>' +
-        '</template>' +
-        '<menu-item name="userInfo" style="width: 20%;text-align: right;"><Icon type="ios-people" />{{currentUser.realname}}</menu-item>' +
-        '<menu-item name="logout" style="width: 10%;text-align: right;"><Icon type="ios-exit-outline" /> Logout</menu-item>' +
-        '</i-menu>',
+    template: `
+        <i-menu mode="horizontal" theme="dark"
+            :active-name="opened"
+            @on-select="onMenuItemSelected">
+            <template v-for="item in menuItems">
+                <menu-item
+                    :name="item.name"
+                    :style="item.style + (item.name===opened ? 'background: rgb(91, 100, 120);' : '')">
+                    <Icon :type="item.icon" /> {{item.text}}
+                </menu-item>
+            </template>
+            <menu-item name="userInfo" style="width: 20%;text-align: right;">
+                <Icon type="ios-people" />{{currentUser.realname}}
+            </menu-item>
+            <menu-item name="logout" style="width: 10%;text-align: right;">
+                <Icon type="ios-exit-outline" /> Logout
+            </menu-item>
+        </i-menu>
+    `,
     props: ['opened'],
     data: function () {
         return {
-            currentUser: JSON.parse(SinriQF.cookies.getCookie("DatabaseHubUser")),
+            currentUser: JSON.parse(SinriQF.cookies.getCookie('DatabaseHubUser')),
             menuItems: [
                 {
-                    name: "indexPage",
-                    style: "width: 30%;text-align: left;",
-                    icon: "ios-nuclear",
-                    text: "DatabaseHub"
+                    name: 'indexPage',
+                    style: 'width: 30%;text-align: left;',
+                    icon: 'ios-nuclear',
+                    text: 'DatabaseHub'
                 },
                 {
-                    name: "requestPage",
-                    style: "width: 10%;text-align: center;",
-                    icon: "ios-nuclear",
-                    text: "Requests"
+                    name: 'requestPage',
+                    style: 'width: 10%;text-align: center;',
+                    icon: 'ios-nuclear',
+                    text: 'Requests'
                 },
                 {
-                    name: "taskPage",
-                    style: "width: 10%;text-align: center;",
-                    icon: "ios-nuclear",
-                    text: "Tasks"
+                    name: 'taskPage',
+                    style: 'width: 10%;text-align: center;',
+                    icon: 'ios-nuclear',
+                    text: 'Tasks'
                 },
                 {
-                    name: "configPage",
-                    style: "width: 10%;text-align: center;",
-                    icon: "ios-nuclear",
-                    text: "Configure"
+                    name: 'configPage',
+                    style: 'width: 10%;text-align: center;',
+                    icon: 'ios-nuclear',
+                    text: 'Configure'
                 },
                 {
-                    name: "queryPage",
-                    style: "width: 10%;text-align: center;",
-                    icon: "ios-nuclear",
-                    text: "Quick Query"
-                },
+                    name: 'queryPage',
+                    style: 'width: 10%;text-align: center;',
+                    icon: 'ios-nuclear',
+                    text: 'Quick Query'
+                }
             ]
         };
     },
     methods: {
-        ensureLogin: function () {
+        ensureLogin () {
             if (!SinriQF.cookies.getCookie(SinriQF.config.TokenName)) {
-                window.location.href = "login.html";
+                window.location.href = 'login.html';
             }
         },
-        onMenuItemSelected: function (name) {
-            console.log("onMenuItemSelected", name);
+        onMenuItemSelected (name) {
+            console.log('onMenuItemSelected', name);
             switch (name) {
-                case "indexPage":
-                    window.location.href = "index.html";
+                case 'indexPage':
+                    window.location.href = 'index.html';
                     break;
-                case "requestPage":
-                    window.location.href = "index.html";
+                case 'requestPage':
+                    window.location.href = 'index.html';
                     break;
-                case "taskPage":
-                    window.location.href = "index.html";
+                case 'taskPage':
+                    window.location.href = 'index.html';
                     break;
-                case "configPage":
-                    window.location.href = "configure.html";
+                case 'configPage':
+                    window.location.href = 'configure.html';
                     break;
-                case "queryPage":
-                    window.location.href = "index.html";
+                case 'queryPage':
+                    window.location.href = 'index.html';
                     break;
-                case "userInfo":
+                case 'userInfo':
                     break;
-                case "logout":
-                    SinriQF.cookies.cleanCookie("DatabaseHubUser");
+                case 'logout':
+                    SinriQF.cookies.cleanCookie('DatabaseHubUser');
                     SinriQF.cookies.cleanCookie(SinriQF.config.TokenName);
-                    window.location.href = "login.html";
+                    window.location.href = 'login.html';
                     break;
             }
         }
     },
-    mounted: function () {
-        SinriQF.config.TokenName = "database_hub_token";
+    mounted () {
+        SinriQF.config.TokenName = 'database_hub_token';
         SinriQF.config.vueInstance = this;
 
         this.ensureLogin();
