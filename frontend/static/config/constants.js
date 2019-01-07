edc131e83691124ae98a7460b65774c94a77b16a
Vinci <ivill@live.com>
2019-01-07 10:53:26 +0800

    modified icon

--- frontend/static/config/constants.js
+++ frontend/static/config/constants.js
@@ -6,10 +6,10 @@ const CONSTANTS = {
         'DDL'
     ],
     APPLICATION_TYPES_ICON_TYPE_MAP: {
-        READ: 'md-color-filter',
-        MODIFY: 'md-code-working',
-        EXECUTE: 'ios-build',
-        DDL: 'md-filing'
+        READ: 'ios-search',
+        MODIFY: 'ios-create',
+        EXECUTE: 'md-arrow-dropright-circle',
+        DDL: 'md-barcode'
     },
     APPLICATION_STATUS: [
         'APPLIED',
@@ -22,12 +22,12 @@ const CONSTANTS = {
     ],
     APPLICATION_STATUS_ICON_TYPE_MAP: {
         APPLIED: 'ios-send',
-        DENIED: 'ios-thumbs-down',
-        CANCELLED: 'ios-pause',
-        APPROVED: 'ios-thumbs-up',
-        EXECUTING: 'ios-flash',
-        DONE: 'md-checkmark',
-        ERROR: 'md-close'
+        CANCELLED: 'md-arrow-round-back',
+        DENIED: 'md-close',
+        APPROVED: 'md-checkmark',
+        EXECUTING: 'md-arrow-round-forward',
+        DONE: 'ios-checkmark-circle',
+        ERROR: 'ios-close-circle'
     },
     APPLICATION_STATUS_TAG_COLOR_MAP: {
         APPLIED: 'blue',
