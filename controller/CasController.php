abfed718569a1d8acb7bbcb7eb9805dd8a378d00
ctang <ctang1@ibenben.com>
2019-04-16 16:54:03 +0800

    add logout

--- controller/CasController.php
+++ controller/CasController.php
@@ -10,6 +10,9 @@ namespace sinri\databasehub\controller;
 
 
 use sinri\ark\web\implement\ArkWebController;
+use sinri\databasehub\core\HubCore;
+use sinri\databasehub\entity\DingtalkScanLoginSessionEntity;
+use sinri\databasehub\entity\SessionEntity;
 use sinri\databasehub\plugin\LoginPluginWithLeqeeCAS;
 
 class CasController extends ArkWebController
@@ -29,4 +32,39 @@ class CasController extends ArkWebController
             echo $e->getMessage();
         }
     }
+
+    /**
+     * 获取登录配置
+     */
+    public function getLoginConfig()
+    {
+        try {
+            $tp_code = HubCore::getConfig(['aa', 'tp_code'], '');
+            $cas_domain = HubCore::getConfig(['aa', 'domain'], 'https://account-auth-v3.leqee.com');
+            $this->_sayOK(['cas_login_url' => $cas_domain . '/CAS/login?service=' . $tp_code ]);
+        } catch (\Exception $e) {
+            $this->_sayFail($e->getMessage());
+        }
+    }
+
+    /**
+     * 退出登录删除token
+     */
+    public function logoutCallback()
+    {
+        try {
+            $ticket = $this->_readRequest("ticket", '');
+            $tp_token = $this->_readRequest("tp_token", '');
+            if (!empty($tp_token)) {
+                $verify_session = (new DingtalkScanLoginSessionEntity())->getByToken($tp_token);
+                if ($verify_session) {
+                    SessionEntity::disableSession($verify_session->userSessionToken);
+                }
+            }
+            $this->_sayOK();
+        } catch (\Exception $e) {
+            $this->_sayFail($e->getMessage());
+        }
+    }
+
 }
